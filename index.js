6e7209e32d0ee3a272f543ecc3486842d93f4661
Hongbo LU <gitawego@gmail.com>
2017-06-19 23:17:22 +0200

    updated

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 const publicIp = require('public-ip'),
-	CloudFlareAPI = require('cloudflare4'),
-	api = new CloudFlareAPI({
-		email: process.env.EMAIL,
-		key: process.env.API_KEY
-	}),
-	minutes = process.env.UPDATE_TIME,
-	the_interval = minutes * 60 * 1000;
+  CloudFlareAPI = require('cloudflare4'),
+  api = new CloudFlareAPI({
+    email: process.env.EMAIL,
+    key: process.env.API_KEY
+  }),
+  minutes = process.env.UPDATE_TIME,
+  the_interval = minutes * 60 * 1000;
 
 function findRecord(records, type) {
-	let found;
-	records.some((record) => {
-		if (record.type.toUpperCase() === type.toUpperCase()) {
-			found = record;
-			return true;
-		}
-	});
-	return found;
+  let found;
+  records.some((record) => {
+    if (record.type.toUpperCase() === type.toUpperCase()) {
+      found = record;
+      return true;
+    }
+  });
+  return found;
 }
 
 function updateRecord() {
-	publicIp.v4().then(ip => {
-		api.zoneGetAll({
-			name: process.env.ZONE
-		}).then(function (zone) {
-			api.zoneDNSRecordGetAll(zone[0].id, {
-				name: process.env.RECORD
-			}).then(function (records) {
-				const record = findRecord(records, 'A');
-				if (!record) {
-					console.log('record not found');
-					return;
-				}
-				if (record.content.trim() === ('' + ip).trim()) {
-					console.log('same ip, do not update',ip);
-					return;
-				}
-				api.zoneDNSRecordUpdate(zone[0].id, record.id, {
-					type: 'A',
-					content: ip
-				}).then(function (result) {
-					console.log(result);
-				});
-			});
-		});
-	});
+  publicIp.v4().then(ip => {
+    api.zoneGetAll({
+      name: process.env.ZONE
+    }).then(function (zone) {
+      api.zoneDNSRecordGetAll(zone[0].id, {
+        name: process.env.RECORD
+      }).then(function (records) {
+        const record = findRecord(records, 'A');
+        if (!record) {
+          console.log('record not found');
+          return;
+        }
+        if (record.content.trim() === ('' + ip).trim()) {
+          console.log('same ip, do not update', ip);
+          return;
+        }
+        api.zoneDNSRecordUpdate(zone[0].id, record.id, {
+          type: 'A',
+          content: ip
+        }).then(function (result) {
+          console.log(result);
+        });
+      });
+    });
+  });
 }
 
 setInterval(updateRecord, the_interval);
