c5a95844c80c4f957e1b982161c56a9fc2a4914c
Hongbo LU <gitawego@gmail.com>
2018-04-15 23:39:05 +0200

    updated build-rpi.sh

--- build-rpi.sh
+++ build-rpi.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+read -p "going to build image, press [Enter] to continue"
+
 docker build . -f ./docker/raspberry-pi/Dockerfile -t cloudflare-ddns-docker-rpi
+
+docker tag cloudflare-ddns-docker-rpi ichainml/cloudflare-ddns-docker:rpi-latest
+
+read -p "now push to docker, press [Enter] to continue"
+docker push ichainml/cloudflare-ddns-docker:rpi-latest
